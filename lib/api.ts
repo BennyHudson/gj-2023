71fbf438665a24bda62adc60fc49d629066ee5ee
Ben Hudson <ben@wedo.digital>
2023-01-08 19:22:31 +0000

    Updating logic around fetching

--- lib/api.ts
+++ lib/api.ts
@@ -6,7 +6,7 @@ const queryPostType = (postType: string, after?: string) => {
   return {
     query: gql`
       query getAll {
-        ${postType}s(first: 1000, after: "${after}") {
+        ${postType}s(first: 100, after: "${after}") {
           pageInfo {
             hasNextPage
             hasPreviousPage
@@ -27,15 +27,15 @@ const queryPostType = (postType: string, after?: string) => {
 
 export const getAllPosts = async (postType: string): Promise<{cursor: string; node: { slug: string}}[]> => {
   const allPosts = []
-  let hasNextPage = true
+  // let hasNextPage = true
   let after = ''
 
-  while (hasNextPage) {
+  while (allPosts.length < parseInt(process.env.NEXT_PUBLIC_API_REQUEST_VOLUME!)) {
     const posts = await client.query(queryPostType(postType, after))
     if (posts) {
       allPosts.push(...posts.data[`${postType}s`].edges)
       // hasNextPage = posts.data[`${postType}s`].pageInfo.hasNextPage
-      hasNextPage = false
+      // hasNextPage = false
       after = posts.data[`${postType}s`].pageInfo.endCursor
     }
   }
