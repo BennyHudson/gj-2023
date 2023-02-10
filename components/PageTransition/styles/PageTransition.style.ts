35bd35863e4212809aa687d719bc3447e2b7cdf8
Ben Hudson <ben@wedo.digital>
2023-02-10 09:52:08 +0000

    Cleaning up package

--- components/PageTransition/styles/PageTransition.style.ts
+++
@@ -1,7 +0,0 @@
-import styled, { css, FlattenSimpleInterpolation } from 'styled-components'
-
-export const PageTransition = styled.div(
-  (): FlattenSimpleInterpolation => css`
-    overflow: hidden;
-  `,
-)
