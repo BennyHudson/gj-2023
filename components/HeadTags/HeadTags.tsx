ba5186146ccf3fb7bd53886988fedc0ee86173dc
Ben Hudson <ben@wedo.digital>
2023-03-15 23:26:08 +0000

    Fixing the favicon

--- components/HeadTags/HeadTags.tsx
+++ components/HeadTags/HeadTags.tsx
@@ -12,19 +12,19 @@ const HeadTags: FC<HeadTagsProps> = ({ seo }: HeadTagsProps): ReactElement => {
       <link
         rel='apple-touch-icon'
         sizes='180x180'
-        href='https://cdn.thegentlemansjournal.com/wp-content/themes/lydia/assets/favicons/apple-touch-icon.png'
+        href='https://cdn.cms.thegentlemansjournal.com/wp-content/themes/lydia/assets/favicons/apple-touch-icon.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='32x32'
-        href='https://cdn.thegentlemansjournal.com/wp-content/themes/lydia/assets/favicons/favicon-32x32.png'
+        href='https://cdn.cms.thegentlemansjournal.com/wp-content/themes/lydia/assets/favicons/favicon-32x32.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='16x16'
-        href='https://cdn.thegentlemansjournal.com/wp-content/themes/lydia/assets/favicons/favicon-16x16.png'
+        href='https://cdn.cms.thegentlemansjournal.com/wp-content/themes/lydia/assets/favicons/favicon-16x16.png'
       />
       <meta charSet='UTF-8' />
       <meta httpEquiv='x-ua-compatible' content='ie=edge,chrome=1' />
