baa1a9ffa3ba86e9203e1a9e0971b0b3c3fada27
Ben Hudson <ben@wedo.digital>
2023-03-15 09:37:16 +0000

    Updating the FeaturedImageUrl helper

--- helpers/featuredImageUrl.ts
+++ helpers/featuredImageUrl.ts
@@ -1,5 +1,5 @@
 function featuredImageUrl(featuredImage: string): string {
-  // if (featuredImage.includes('cdn.dev')) return featuredImage.replace('cdn.dev', 'cdn')
+  if (featuredImage.includes('cdn.thegentlemansjournal.com')) return featuredImage.replace('cdn', 'cdn.cms')
   // if (featuredImage.includes('cdn.cms')) return featuredImage.replace('cdn.cms', 'cdn')
   // if (featuredImage.includes('dev.thegentlemansjournal.com')) return featuredImage.replace('dev', 'cdn')
   // if (featuredImage.includes('cms.thegentlemansjournal.com')) return featuredImage.replace('cms', 'cdn')
