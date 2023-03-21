07c9f61639b652a6ad8fa3e2b154fee54277ba85
Ben Hudson <ben@wedo.digital>
2023-03-21 22:04:47 +0000

    Fixing the HeadingBlock styles

--- components/ContentBuilder/components/HeadingBlock/styles/HeadingBlock.style.ts
+++ components/ContentBuilder/components/HeadingBlock/styles/HeadingBlock.style.ts
@@ -1,7 +1,19 @@
 import styled, { css, FlattenSimpleInterpolation } from 'styled-components'
 
+import respondTo from '@mixins/respondTo'
+
 export const HeadingBlock = styled.div(
   (): FlattenSimpleInterpolation => css`
-    grid-column: col-start 3 / span 7;
+    grid-column: col-start / span 12;
+    padding: 0 5%;
+
+    ${respondTo.md(css`
+      padding: 0;
+      grid-column: col-start 2 / span 10;
+    `)}
+
+    ${respondTo.lg(css`
+      grid-column: col-start 3 / span 7;
+    `)}
   `,
 )
