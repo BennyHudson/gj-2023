4ad942edc06906919a653821ebfcef03d1dd0cc0
Ben Hudson <ben@wedo.digital>
2023-04-05 19:34:44 +0100

    Adding Facebook Pixel

--- components/Scripts/Scripts.tsx
+++ components/Scripts/Scripts.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element, jsx-a11y/alt-text */
 import React, { ReactElement, FC } from 'react'
 import Script from 'next/script'
 import { useTheme } from 'styled-components'
@@ -11,6 +12,25 @@ const Scripts: FC = (): ReactElement => {
 
   return (
     <>
+      <Script id='facebook-pixel'>
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '467180983664563');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=467180983664563&ev=PageView&noscript=1"
+        />
+      </noscript>
       <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=AW-10918806122" />
       <Script id='googlePixel'>
         {`
