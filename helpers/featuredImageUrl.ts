54a2ac8e997c8aaf245d0a9c4bbafb0753ae56c0
Ben Hudson <ben@wedo.digital>
2023-04-06 15:49:57 +0100

    Updating the featuredImageUrl to use the proper CDN

--- helpers/featuredImageUrl.ts
+++ helpers/featuredImageUrl.ts
@@ -1,10 +1,10 @@
 function featuredImageUrl(featuredImage: string): string {
-  if (featuredImage.includes('//cdn.thegentlemansjournal.com')) return featuredImage.replace('cdn', 'cms')
-  if (featuredImage.includes('cdn.cms')) return featuredImage.replace('cdn.cms', 'cms')
-  // if (featuredImage.includes('dev.thegentlemansjournal.com')) return featuredImage.replace('dev', 'cdn')
-  // if (featuredImage.includes('cms.thegentlemansjournal.com')) return featuredImage.replace('cms', 'cdn')
+  // if (featuredImage.includes('//cms.thegentlemansjournal.com')) return featuredImage.replace('cms', 'cdn')
+  if (featuredImage.includes('cdn.cms')) return featuredImage.replace('cdn.cms', 'cdn')
+  if (featuredImage.includes('dev.thegentlemansjournal.com')) return featuredImage.replace('dev', 'cdn')
+  if (featuredImage.includes('cms.thegentlemansjournal.com')) return featuredImage.replace('cms', 'cdn')
   if (featuredImage.includes('http://thegentlemansjournal.local'))
-    return featuredImage.replace('http://thegentlemansjournal.local', 'https://cms.thegentlemansjournal.com/wp-content')
+    return featuredImage.replace('http://thegentlemansjournal.local', 'https://cdn.thegentlemansjournal.com/wp-content')
   return featuredImage
 }
 
