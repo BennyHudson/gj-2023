7e30d51e25af7e745e29d5ec500ad87a33a00cf3
Ben Hudson <ben@wedo.digital>
2023-04-07 11:13:27 +0100

    Updating image sizes

--- queries/fragments/media.ts
+++ queries/fragments/media.ts
@@ -1,9 +1,9 @@
 export const media = () => {
   return `
     fullSize: sourceUrl
-    postThumb: sourceUrl(size: POST_THUMB)
-    squareThumb: sourceUrl(size: POST_SQUARE)
-    medium: sourceUrl(size: MEDIUM)
-    large: sourceUrl(size: LARGE)
+    postThumb: sourceUrl
+    squareThumb: sourceUrl
+    medium: sourceUrl
+    large: sourceUrl
   `
 }
