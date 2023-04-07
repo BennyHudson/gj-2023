2b7ff0ef68f4401abc9c0618cedc498559a0a4af
Ben Hudson <ben@wedo.digital>
2023-04-07 12:17:23 +0100

    Updating the logo to have priority, using GET for graphql

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -10,9 +10,9 @@ const Logo: FC<LogoProps> = ({ inverse = false, gLogo = false }: LogoProps): Rea
     <Styled.Logo gLogo={gLogo}>
       <Styled.LogoLink href='/'>
         {gLogo ? (
-          <Image src={inverse ? '/svg/logo-small-inverse.svg' : '/svg/logo-small.svg'} width={30} height={30} alt={'Gentleman\'s Journal'} />
+          <Image priority src={inverse ? '/svg/logo-small-inverse.svg' : '/svg/logo-small.svg'} width={30} height={30} alt={'Gentleman\'s Journal'} />
         ) : (
-          <Image src={inverse ? '/svg/logo-inverse.svg' : '/svg/logo.svg'} width={230} height={36} alt={'Gentleman\'s Journal'} />
+          <Image priority src={inverse ? '/svg/logo-inverse.svg' : '/svg/logo.svg'} width={230} height={36} alt={'Gentleman\'s Journal'} />
         )}
       </Styled.LogoLink>
     </Styled.Logo>
