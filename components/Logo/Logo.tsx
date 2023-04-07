3b914ca8c0636763467b49a04b2ee092697a45cd
Ben Hudson <ben@wedo.digital>
2023-04-07 19:25:47 +0100

    Linting

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -9,12 +9,6 @@ const Logo: FC<LogoProps> = ({ inverse = false, gLogo = false }: LogoProps): Rea
   return (
     <Styled.Logo gLogo={gLogo}>
       <Styled.LogoLink href='/' $inverse={inverse} $gLogo={gLogo} />
-        {/* {gLogo ? (
-          <Image priority src={inverse ? '/svg/logo-small-inverse.svg' : '/svg/logo-small.svg'} width={30} height={30} alt={'Gentleman\'s Journal'} />
-        ) : (
-          <Image priority src={inverse ? '/svg/logo-inverse.svg' : '/svg/logo.svg'} width={230} height={36} alt={'Gentleman\'s Journal'} />
-        )}
-      </Styled.LogoLink> */}
     </Styled.Logo>
   )
 }
