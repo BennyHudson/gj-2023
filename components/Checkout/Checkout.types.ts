e5f5155ef012b5f3053b7ace43fc807c338ff720
Ben Hudson <ben@wedo.digital>
2023-04-13 09:04:27 +0100

    More linting

--- components/Checkout/Checkout.types.ts
+++ components/Checkout/Checkout.types.ts
@@ -1,7 +1,7 @@
 import type { PaymentIntent } from '@stripe/stripe-js'
 import type { ReactNode } from 'react'
 
-import type { BillingAddress } from '@typings/BillingAddress.types'
+import type { BillingAddressProps } from '@typings/BillingAddress.types'
 import type { ShippingAddress } from '@typings/ShippingAddress.types'
 import type { Voucher } from '@typings/Voucher.types'
 
@@ -14,7 +14,7 @@ export interface CheckoutState {
   activePanel: number
   message: string
   checkoutForm: {
-    billing?: BillingAddress
+    billing?: BillingAddressProps
     shipping?: ShippingAddress
     voucher?: Voucher
   }
