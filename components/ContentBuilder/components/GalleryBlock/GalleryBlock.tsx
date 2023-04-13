1f7f31807bb31a548656e0c8f09c16048380ba9b
Ben Hudson <ben@wedo.digital>
2023-04-13 20:39:29 +0100

    More fixes

--- components/ContentBuilder/components/GalleryBlock/GalleryBlock.tsx
+++ components/ContentBuilder/components/GalleryBlock/GalleryBlock.tsx
@@ -1,21 +1,25 @@
 import Image from 'next/image'
 import type { FC, ReactElement } from 'react'
 import React from 'react'
+import { useTheme } from 'styled-components'
 
 import RawHtmlWrapper from '@components/RawHtmlWrapper'
 
 import featuredImageUrl from '@helpers/featuredImageUrl'
 
+import type { Theme } from '@themes/gjTheme/gjTheme.types'
+
 import type { GalleryBlockProps } from './GalleryBlock.types'
 import * as Styled from './styles/GalleryBlock.style'
 
 const GalleryBlock: FC<GalleryBlockProps> = ({ gallery }: GalleryBlockProps): ReactElement => {
+  const { containerWidth } = useTheme() as Theme
   return (
     <Styled.GalleryBlock imageCount={gallery.length}>
       {gallery.map((image, index) => {
         return (
           <div key={index}>
-            <Image src={featuredImageUrl(image.large)} width={900} height={900} alt={image.caption || ''} />
+            <Image src={featuredImageUrl(image.large)} width={containerWidth} height={containerWidth} alt={image.caption || ''} />
             {image.caption && (
               <Styled.Caption>
                 <RawHtmlWrapper content={image.caption} />
